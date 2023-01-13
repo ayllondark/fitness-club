dc97f5de9f0769720d323436e51a42b6c07e8fe1
Miguel Ayllón <ayllondark@gmail.com>
2023-01-13 12:30:48 -0500

    carga img github

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.css';
-import Github from "../../assets/Github.png";
+import Github from "../../assets/github.png";
 import Instagram from "../../assets/instagram.png";
 import LinkedIn from "../../assets/linkedin.png";
 import Logo from "../../assets/logo.png";
